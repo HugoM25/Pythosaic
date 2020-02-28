a648c18e7970857b565d138f4a28e2d1997d141d
HugoM25 <58184548+HugoM25@users.noreply.github.com>
2020-02-28 23:22:50 +0100

    Add files via upload

---
+++ ImgtoPhotomosaics_GifEdit/ConvertToGIF.py
@@ -0,0 +1,55 @@
+﻿import ConvertToIMG
+import CreateDataSet
+import os
+from PIL import Image
+import shutil
+from collections import *
+import ConvertToIMG
+
+def ExtractingFrames(thegif):
+    outfolder = "extractedFrames"
+    infolder = "doneFrames"
+    try :
+        shutil.rmtree(outfolder +"/")
+        shutil.rmtree(infolder +"/")
+    except :
+        pass
+    os.makedirs(outfolder + "/")
+    os.makedirs(infolder + "/")
+    frame = Image.open(thegif)
+    nframes = 0
+    while frame :
+        frame.save("%s/%s-%s.png" % (outfolder, os.path.basename(thegif), nframes), "PNG")
+        nframes += 1
+        try :
+            frame.seek(nframes)
+        except EOFError:
+            break;
+    return True
+
+def Concatenate(nameofGif):
+    folderwithIMG = "doneFrames"
+    outfolder = "extractedFrames"
+    imageList = []
+    listoffiles = os.listdir( folderwithIMG+ "/")
+    number_files = len(listoffiles)
+
+    for i in range(0, number_files):
+        im = Image.open("%s/%s-%s.png" % (folderwithIMG, os.path.basename(nameofGif), i)).convert("RGBA")
+        imageList.append(im)
+    imageList[0].save("gifFinal.gif", save_all=True, append_images = imageList[1:], duration=100, loop = 10)
+
+
+if __name__ == "__main__" :
+
+    datasetname = input("The name of your dataset ( with the extension )")
+    im = Image.open(datasetname).convert("RGBA")
+    nameofGif = input("The name of the gif file :")
+    outfolder = "extractedFrames"
+    ExtractingFrames(nameofGif)
+    listoffiles = os.listdir( outfolder+ "/")
+    number_files = len(listoffiles)
+    for i in range(0, number_files) :
+        image_finale = ConvertToIMG.ConverttheIMG(datasetname, im,Image.open("%s/%s-%s.png" % ("extractedFrames", os.path.basename(nameofGif), i)).convert("RGBA"))
+        image_finale.save("%s/%s-%s.png" % ("doneFrames", os.path.basename(nameofGif), i))
+    Concatenate(nameofGif)
---
+++ ImgtoPhotomosaics_GifEdit/MakeADataSet.py
@@ -0,0 +1,40 @@
+﻿from PIL import Image
+import os
+
+def CreateImageSet():
+    dimension = 50
+    listoffiles = os.listdir("DataToUse/")
+    number_files = len(listoffiles)
+    print(number_files)
+    datadim = GuessTwoInt(number_files)
+    image_finale = Image.new("RGB", (dimension*datadim[0], dimension*datadim[1]), color = "black")
+    rangexmax = datadim[0]
+    rangex = 0
+    rangey = 0
+    for i in range(0,number_files) :
+        try :
+            img = Image.open("DataToUse/meme"+str(i) + ".png")
+        except :
+            img = Image.open("DataToUse/meme" + str(i) + ".jpg")
+
+        img = img.resize((dimension,dimension), Image.ANTIALIAS)
+
+        if rangex >= rangexmax :
+            rangey += 1
+            rangex = 0
+        image_finale.paste(img, (dimension*rangex, dimension * rangey))
+        rangex +=1
+    image_finale.save("finalset.png")
+
+def GuessTwoInt(number):
+    num1 = 0
+    num2 = 0
+    for i in range(1,10) :
+        if number % i == 0 :
+            num1 = int(i)
+            num2 = int(number/i)
+    return num1, num2
+
+
+if __name__ == "__main__" :
+    CreateImageSet()
